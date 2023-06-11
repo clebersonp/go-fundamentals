ca1f374e093954a86c8202cdf1ac03449a83f771
Cleberson <pauluci.cleberson@gmail.com>
2023-06-11 17:10:04 -0300

    Examples of go generate cli

---
+++ go-generate/go.mod
@@ -0,0 +1,3 @@
+module github.com/clebersonp/go-generate
+
+go 1.20
---
+++ go-generate/main.go
@@ -0,0 +1,11 @@
+package main
+
+// The main Go binary has a sub-command called generate which is used to trigger code generation tools (like stringer).
+
+// execute this code generate in the terminal just typing `go generate`
+// or to execute all go:generate for all sub-packages: $ go generate ./...
+
+//go:generate echo "Hello World"
+func main() {
+
+}
